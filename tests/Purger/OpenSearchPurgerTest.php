3325675d6fc761ef939c06b882db9a2b47a5f7e9
Hugo Gonçalves <hugo.goncalves@kununu.com>
2025-01-21 12:49:52 +0000

    Fix OpenSearchExecutor tests
    Add tests to OpenSearchFixturesLoader
    
    Add tests to OpenSearchPurger

---
+++ tests/Purger/OpenSearchPurgerTest.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace Kununu\DataFixtures\Tests\Purger;
+
+use Kununu\DataFixtures\Purger\OpenSearchPurger;
+use Kununu\DataFixtures\Purger\PurgerInterface;
+use OpenSearch\Client;
+use OpenSearch\Namespaces\IndicesNamespace;
+use PHPUnit\Framework\MockObject\MockObject;
+use stdClass;
+
+final class OpenSearchPurgerTest extends AbstractPurgerTestCase
+{
+    private MockObject&Client $client;
+
+    public function testPurge(): void
+    {
+        $indices = $this->createMock(IndicesNamespace::class);
+        $indices
+            ->expects(self::exactly(2))
+            ->method('refresh')
+            ->with(['index' => 'my_index']);
+
+        $this->client
+            ->expects(self::any())
+            ->method('indices')
+            ->willReturn($indices);
+
+        $this->client
+            ->expects(self::once())
+            ->method('deleteByQuery')
+            ->with(
+                [
+                    'index'     => 'my_index',
+                    'body'      => [
+                        'query' => [
+                            'match_all' => new stdClass(),
+                        ],
+                    ],
+                    'conflicts' => 'proceed',
+                ]
+            )
+            ->willReturn(true);
+
+        $this->purger->purge();
+    }
+
+    protected function setUp(): void
+    {
+        $this->client = $this->createMock(Client::class);
+
+        parent::setUp();
+    }
+
+    protected function getPurger(): PurgerInterface
+    {
+        return new OpenSearchPurger($this->client, 'my_index');
+    }
+}
