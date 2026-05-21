b2751a0c3d6797503d84879e3fc302a6a3e562f9
Hugo Gonçalves <hugo.goncalves@kununu.com>
2026-05-21 12:15:17 +0100

    Bump GitHub Actions versions
    - Bump GitHub Actions versions
    - Bump PHPUnit to ^13.1

--- tests/Purger/OpenSearchPurgerTest.php
+++ tests/Purger/OpenSearchPurgerTest.php
@@ -23,7 +23,7 @@ final class OpenSearchPurgerTest extends AbstractPurgerTestCase
             ->with(['index' => 'my_index']);
 
         $this->client
-            ->expects($this->any())
+            ->expects($this->exactly(2))
             ->method('indices')
             ->willReturn($indices);
 
